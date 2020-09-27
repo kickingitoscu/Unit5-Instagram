2a63067051a3fbbc0848b8c38cfe7820dabfe76a
kickingitoscu <mduarte@csumb.edu>
2020-09-27 16:39:11 -0700

    Initial Commit

---
+++ Unit5-Instagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Unit5-Instagram
+//
+//  Created by Mariana Duarte on 9/27/20.
+//  Copyright © 2020 Codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
